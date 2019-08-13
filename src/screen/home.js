a62b423dc10c94608e154580eb8190ce969dbe3b
ganang wahyu wicaksono <gananggww@gmail.com>
2019-08-14 01:34:17 +0700

    fetch api

--- src/screen/home.js
+++ src/screen/home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
   Text,
   Button,
-  ToastAndroid
+  ToastAndroid,
+  ListView
 } from 'react-native';
 
+import axios from 'axios'
+
 const Home = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const {data} = await axios({method:'GET', url: 'https://jsonplaceholder.typicode.com/todos/'});
+          setData(data);
+        };
+        fetchData();
+    }, []);
+
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Home {JSON.stringify(data)}</Text>
         </View>
     )
 }
